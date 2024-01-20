4aba86ceda45f32e1949950807393255dba2cd05
waltermatulich <wmatulich231@gmail.com>
2024-01-19 22:49:23 -0800

    Added lisence codes to Markdown.js file.

---
+++ README.md
@@ -0,0 +1,35 @@
+
+    #readme-generator
+
+    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    
+    MIT
+
+    Description:
+    
+    Quality Javascript code that generates a README
+    
+    Table of Contents:
+    
+    Installation:
+    
+    To install, run the following command⬇️
+
+    npm install
+
+    Usage:
+    README with a description of this code and a contact link.
+
+    Contributing:
+    Constributing took hours of Javascript code that I newly learned.
+
+    Test:
+
+    undefined
+
+    This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+    If you have questions, please feel free to contact me.
+    - Email: @wmatulich231@gmail.com
+    - GitHub Account: waltermatulich https://github.com/waltermatulich
+    
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,36 +1,73 @@
+// Function to generate license badge and notice
+function generateLicenseBadgeAndNotice(license) {
+    let badge;
+    let notice;
+
+    switch (license) {
+        case "MIT":
+            badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            notice = "This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.";
+            break;
+        case "APACHE":
+            badge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+            notice = "This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.";
+            break;
+        case "GPL":
+            badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+            notice = "This project is licensed under the GNU General Public License v3.0 - see the [LICENSE](LICENSE) file for details.";
+            break;
+        case "None":
+            // If no license is chosen, no badge and notice
+            badge = "";
+            notice = "No license specified.";
+            break;
+        default:
+            badge = "";
+            notice = "";
+            break;
+    }
+
+    return { badge, notice };
+}
+
 // Generating markdown data
 module.exports = function generateMarkdown(data) {
+    // Generate license badge and notice based on the chosen license
+    const { badge, notice } = generateLicenseBadgeAndNotice(data.license);
+
     return `
     #${data.title}
+
+    ${badge}
     
     ${data.license}
 
-    Description
+    Description:
     
     ${data.description}
     
     Table of Contents:
     
-    Installation
+    Installation:
     
     To install, run the following command⬇️
 
     ${data.installation}
 
-    Usage
+    Usage:
     ${data.usage}
 
-    Contributing
+    Contributing:
     ${data.contributing}
 
-    Tests
+    Test:
 
-    ${data.tests}
+    ${data.test}
 
-    Questions
+    ${notice}
 
-    ${data.email}
-    ${data.github}
-
-    `
+    If you have questions, please feel free to contact me!
+    - Email: @${data.email}
+    - GitHub Account: ${data.github} https://github.com/waltermatulich
+    `;
 };
